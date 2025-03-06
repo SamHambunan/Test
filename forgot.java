8e77fac29c167bb55a6479970460d853bf7924fb
Sam Hambunan <samhambunan@gmail.com>
2025-03-06 14:09:45 +0800

    Version 1 overwrite no git ignore

---
+++ forgot.java
@@ -0,0 +1,11 @@
+
+public class forgot {
+
+    public static void main(String[] args) {
+        System.out.println("Ooops i forgot to add this");
+        System.out.println("Wow a new version for real");
+        System.out.println("Added new version 1 update");
+       
+        
+    }
+}
---
+++ src/status.java
@@ -0,0 +1,8 @@
+package src;
+
+public class status {
+
+    public static void main(String[] args) {
+        System.out.println("STATUS VERSION 1 brah UPDATED");
+    }
+}
