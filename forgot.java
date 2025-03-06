e4b4e7bd110ff196bfc54f0e13ceff6942a3ebeb
Sam Hambunan <samhambunan@gmail.com>
2025-03-06 14:13:28 +0800

    version 2 without src

--- forgot.java
+++ forgot.java
@@ -4,8 +4,9 @@ public class forgot {
     public static void main(String[] args) {
         System.out.println("Ooops i forgot to add this");
         System.out.println("Wow a new version for real");
-        System.out.println("Added new version 1 update");
-       
-        
+        System.out.println("Added new version 2 update");
+        System.out.println("Added new version 2 update");
+        // new update
+
     }
 }
--- helloWorld.java
+++ helloWorld.java
@@ -3,6 +3,6 @@ public class helloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello brah");
-        // this is version 1 updated
+        // this is version 2 updated
     }
 }
